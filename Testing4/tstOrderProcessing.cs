c721f56021b41c6cce3fec2d17b8bb617076be6c
Hassan Choudhury <hchoudhury20@hotmail.com>
2024-05-02 10:26:20 +0100

    add test class for order proccesing

---
+++ Testing4/tstOrderProcessing.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstOrderProcessing
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsadress AnAddress = new clsadress();
+
+            Assert.IsNotNull(AnAddress);
+
+        }
+    }
+}
