ccc0c8b8cb046bf1ecee1c1200e5ef74c6a5d288
Hassan Choudhury <hchoudhury20@hotmail.com>
2024-05-07 14:14:03 +0100

    add test cases for order class

---
+++ ClassLibrary/clsOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+        public int OrderId { get; set; }
+        public int ISBN { get; set; }
+        public int UserId { get; set; }
+        public bool? IsValid { get; set; } = true;
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+        public string Status { get; set; } = "Started";
+    }
+}
--- Testing4/tstOrderProcessing.cs
+++ Testing4/tstOrderProcessing.cs
@@ -1,18 +1,44 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 namespace Testing4
 {
     [TestClass]
-    public class tstOrderProcessing
+    public class tstOrder
     {
         [TestMethod]
         public void InstanceOK()
         {
-            clsadress AnAddress = new clsadress();
+            clsOrder AnOrder = new clsOrder();
+            Assert.IsNotNull(AnOrder);
+        }
 
-            Assert.IsNotNull(AnAddress);
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.CreatedAt = TestData;
+            Assert.AreEqual(AnOrder.CreatedAt, TestData);
+        }
 
+        [TestMethod]
+        public void OrderIdPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderId = TestData;
+            Assert.AreEqual(AnOrder.OrderId, TestData);
+        }
+
+        [TestMethod]
+        public void isStarrted()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "Started";
+            AnOrder.Status = TestData;
+            Assert.AreEqual(AnOrder.Status, TestData);
         }
     }
 }
