15095b66558c29cbd5eb123f28e3d8357f6c787e
adamv24 <adamvali242@gmail.com>
2024-05-08 14:50:14 +0100

    I've been cooking

--- AdminSystem/StockManagementSystemDataEntry.aspx.cs
+++ AdminSystem/StockManagementSystemDataEntry.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,5 +11,32 @@ public partial class _1_DataEntry : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged1(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOk_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsStock
+        clsStock AStock = new clsStock();
+        //capture the book name
+        AStock.BookName = txtBookName.Text;
+        //store the book name in the session object
+        Session["AStock"] = AStock;
+        //navigate to the view page
+        Response.Redirect("StockManagementSystemViewer.aspx");
     }
 }
