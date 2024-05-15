2578e35ad721b375db34024250ec83adfd10f872
adamv24 <adamvali242@gmail.com>
2024-05-15 16:50:42 +0100

    presentation layer complete

--- AdminSystem/StockManagementSystemList.aspx.cs
+++ AdminSystem/StockManagementSystemList.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,26 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // if this is the first time the page is displayed
+        if(IsPostBack == false)
+        {
+            //update the list box
+            DisplayStock();
+        }
     }
+
+    void DisplayStock()
+    {
+        //create an instance of the stock collection
+        clsStockCollection Stocks = new clsStockCollection();
+        //set the data source to the list of addressin the collection
+        lstStockList.DataSource = Stocks.StockList;
+        //set the name of the primary key
+        lstStockList.DataValueField = "BookAuthor";
+        //set the data field to display
+        lstStockList.DataValueField = "BookName";
+        //bind the data to the list
+        lstStockList.DataBind();
+    }
+
 }
