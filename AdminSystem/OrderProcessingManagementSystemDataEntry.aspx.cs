84e3894ca6f3bbf4173ff092166770adc17dd806
Hassan Choudhury <hchoudhury20@hotmail.com>
2024-05-15 17:07:48 +0100

    make button

--- AdminSystem/OrderProcessingManagementSystemDataEntry.aspx.cs
+++ AdminSystem/OrderProcessingManagementSystemDataEntry.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,8 +14,60 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
-    protected void txtItemId_TextChanged(object sender, EventArgs e)
+    protected void TextBox3_TextChanged(object sender, EventArgs e)
     {
 
     }
+
+    protected void TextBox4_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox4_TextChanged1(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        {
+            // Create an instance of clsOrder
+            ClassLibrary.clsOrder order = new ClassLibrary.clsOrder();
+
+            // Create a variable to store the primary key
+            Int32 OrderId;
+
+            // Create a variable to store the result of the find operation
+            Boolean Found = false;
+
+            // Get the primary key entered by the user
+            OrderId = Convert.ToInt32(txtOrderId.Text);
+
+            // Find the record
+            Found = order.Find(OrderId);
+
+            // If found
+            if (Found == true)
+            {
+                // Display the values of the properties in the form
+                txtISBN.Text = order.ISBN.ToString();
+                txtUserId.Text = order.UserId.ToString();
+                txtStatus.Text = order.Status;
+            }
+            else
+            {
+                // If the order was not found, handle it here
+                lblStatusValue.Text = "Order not found";
+            }
+        }
+    }
 }
+
+      
