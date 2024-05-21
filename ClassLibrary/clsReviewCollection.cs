b19a1f492a271caa80c42f8a25d6a90538b1b2d4
Andrej Gazi <Andrejgazi25@gmail.com>
2024-05-21 19:05:53 +0100

    done

---
+++ ClassLibrary/clsReviewCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsReviewCollection
+    {
+        public clsReviewCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblBookReviews_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+                clsReview anReview = new clsReview();
+                anReview.UserId = Convert.ToInt32(DB.DataTable.Rows[0]["UserId"]);
+                anReview.ReviewId = Convert.ToInt32(DB.DataTable.Rows[0]["ReviewId"]);
+                anReview.RatingId = Convert.ToString(DB.DataTable.Rows[0]["Rating"]);
+                anReview.BookId = Convert.ToInt32(DB.DataTable.Rows[0]["BookId"]);
+                anReview.Active = Convert.ToBoolean(DB.DataTable.Rows[0]["ReviewSubmited"]);
+                anReview.Text = Convert.ToString(DB.DataTable.Rows[0]["ReviewText"]);
+                anReview.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[0]["DateAdded"]);
+                mReviewlist.Add(anReview);
+                Index++;
+            }
+        }
+
+        //private data member for the list
+        List<clsReview> mReviewlist = new List<clsReview>();
+        public List<clsReview> ReviewList { get
+            {
+                return mReviewlist;
+            }
+            set
+            {
+                mReviewlist = value;
+            }
+        }
+        public int Count { get
+            {
+                return mReviewlist.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsReview ThisReview { get; set; }
+
+    }
+
+
+}
---
+++ Testing3/tstReviewCollection.cs
@@ -0,0 +1,73 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstReviewCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsReviewCollection allReview = new clsReviewCollection();
+            Assert.IsNotNull(allReview);
+        }
+
+        [TestMethod]
+        public void ReviewListOK()
+        {
+            clsReviewCollection allReview = new clsReviewCollection();
+            List<clsReview> TestList = new List<clsReview>();
+            clsReview TestItem = new clsReview();
+
+            TestItem.Active = true;
+            TestItem.UserId = 1;
+            TestItem.ReviewId = 1;
+            TestItem.BookId = 1;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.RatingId = "3";
+            TestItem.Text = "1";
+            TestList.Add(TestItem);
+            allReview.ReviewList = TestList;
+            Assert.AreEqual(allReview.ReviewList, TestList);
+        }
+
+        [TestMethod]
+        public void ThisReviewPropertyOK()
+        {
+            clsReviewCollection allReview = new clsReviewCollection();
+            clsReview TestReview = new clsReview();
+            TestReview.Active = true;
+            TestReview.UserId = 1;
+            TestReview.ReviewId = 1;
+            TestReview.BookId = 1;
+            TestReview.DateAdded = DateTime.Now;
+            TestReview.RatingId = "3";
+            TestReview.Text = "1";
+           
+            allReview.ThisReview = TestReview;
+            Assert.AreEqual(allReview.ThisReview, TestReview);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            clsReviewCollection allReview = new clsReviewCollection();
+            List<clsReview> TestList = new List<clsReview>();
+            clsReview TestItem = new clsReview();
+
+            TestItem.Active = true;
+            TestItem.UserId = 1;
+            TestItem.ReviewId = 1;
+            TestItem.BookId = 1;
+            TestItem.DateAdded = DateTime.Now;
+            TestItem.RatingId = "3";
+            TestItem.Text = "1";
+            TestList.Add(TestItem);
+            allReview.ReviewList = TestList;
+            Assert.AreEqual(allReview.Count, TestList.Count);
+        }
+    }
+}
