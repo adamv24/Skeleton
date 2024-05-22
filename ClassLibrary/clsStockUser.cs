cc3916c3d65141b1189495f0c98fc6919fa152bb
adamv24 <adamvali242@gmail.com>
2024-05-22 21:55:57 +0100

    need to to complete from finduser method - page 10

---
+++ ClassLibrary/clsStockUser.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsStockUser
+    {
+        private Int32 mUserID;
+        private string mUserName;
+        private string mPassword;
+        private string mDepartment;
+
+        public int UserID
+        {
+            get
+            {
+                //return private data member
+                return mUserID;
+            }
+            set
+            {
+                //set the private data
+                mUserID = value;
+            }
+        }
+
+
+        public string UserName {
+            get
+            {
+                return mUserName;
+            }
+            set
+            {
+                mUserName = value;
+            }
+        }
+
+
+        public string Password {
+            get
+            {
+                return mPassword;
+            }
+            set 
+            { 
+                mPassword = value; 
+            }
+        }
+        public string Department 
+        {
+            get
+            {
+                return mDepartment;
+            }
+            set
+            { 
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
