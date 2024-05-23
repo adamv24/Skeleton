6b12fa28dab2073c7935f24930938afe254ae09a
adamv24 <adamvali242@gmail.com>
2024-05-23 10:27:06 +0100

    login page works

---
+++ AdminSystem/StockManagementSystemLogin.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="StockManagementSystemLogin.aspx.cs" Inherits="StockManagementSystemLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        <p>Are you sure you want to delete this record?</p>
+
+        </div>
+        <asp:Label ID="lblUserName" runat="server" Text="UserName   "></asp:Label>
+        <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="lblPassword" runat="server" Text="Password   "></asp:Label>
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password"></asp:TextBox>
+            <asp:Label ID="lblError" runat="server" ForeColor="#FF3300"></asp:Label>
+        </p>
+        <asp:Button ID="btnLogin" runat="server" OnClick="btnLogin_Click" Text="Login" />
+        <asp:Button ID="lblCancel" runat="server" Text="Cancel" />
+    </form>
+</body>
+</html>
---
+++ AdminSystem/StockManagementSystemLogin.aspx.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class StockManagementSystemLogin : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        //create an instance of the stock user class
+        clsStockUser AnUser = new clsStockUser();
+        //create the variable tp store the username and password
+        string UserName = txtUserName.Text;
+        string Password = txtPassword.Text;
+        //create a variable to stpre the result of the find user fields operation
+        Boolean Found = false;
+        //get the username entered by the user
+        UserName = Convert.ToString(txtUserName.Text);
+        //get the password entered by the user
+        Password = Convert.ToString(txtPassword.Text);
+        //find the record
+        Found = AnUser.FindUser(UserName, Password);
+        //if username and/or password is empty
+        if (txtUserName.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter a username";
+        }
+        else if (txtPassword.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter a password";
+        }
+        //if found
+        else if (Found == true)
+        {
+            //redirect to the list page
+            Response.Redirect("StockManagementSystemList.aspx");
+        }
+        else if (Found == false)
+        {
+            //record the error
+            lblError.Text = "Login details are incorrect. Please try again";
+        }
+
+    }
+}
