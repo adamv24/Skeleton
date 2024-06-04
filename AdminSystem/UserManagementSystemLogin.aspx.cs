507ee6a17d9a800baf7c7ec4186c3c5f6f9431ea
Hassan Choudhury <hchoudhury20@hotmail.com>
2024-06-04 15:51:16 +0100

    mkae log on page

---
+++ AdminSystem/UserManagementSystemLogin.aspx.cs
@@ -0,0 +1,65 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UserManagementSystemLogin : System.Web.UI.Page
+{
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        //create an instance of the stock user class
+        clsStockUser AnUser = new clsStockUser();
+        //create the variable tp store the username and password
+        string UserName = txtUserName.Text;
+        string Password = txtPassword.Text;
+        //create a variable to stpre the result of the find user fields operation
+        Boolean Found = false;
+        //get the username entered by the user
+        UserName = Convert.ToString(txtUserName.Text);
+        //get the password entered by the user
+        Password = Convert.ToString(txtPassword.Text);
+        //find the record
+        Found = AnUser.FindUser(UserName, Password);
+        //Add a session to capture the username
+        Session["AnUser"] = AnUser;
+        //if username and/or password is empty
+        if (txtUserName.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter a username";
+        }
+        else if (txtPassword.Text == "")
+        {
+            //record the error
+            lblError.Text = "Enter a password";
+        }
+        //if found
+        else if (Found == true)
+        {
+            //redirect to the list page
+            Response.Redirect("OrderProcessingManagementSystemList.aspx");
+        }
+        else if (Found == false)
+        {
+            //record the error
+            lblError.Text = "Login details are incorrect. Please try again";
+        }
+
+    }
+
+    protected void lblCancel_Click(object sender, EventArgs e)
+    {
+        //redirect to the main menu
+        Response.Redirect("TeamMainMenu.aspx");
+    }
+
+}
